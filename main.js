533ea4c52d448e4bc516e5225997ae7863fce860
konics556 <kanishkpatidar18@gmail.com>
2021-07-26 11:51:06 +0530

    changed ui and alert ordering

--- main.js
+++ main.js
@@ -1,166 +1,83 @@
 function onSave(event) {
     event.preventDefault();
-    const form = document.getElementById('form');
-    let invalidFields = [];
-    let focusField = null;
-    let isTncAccepted = true;
+    const form = document.getElementById('employee-form');
+    let message;
 
-    if( !validateFirstName(form[0]) ) {
-        invalidFields.push("First Name");
-        if(focusField === null){
-            focusField = form[0];
-        }
-    }
-    if( !validateLastName(form[1]) ) {
-        invalidFields.push("Last Name");
-        if(focusField === null){
-            focusField = form[1];
-        }
-    }
-    if( !validateGender(form[2], form[3]) ) {
-        invalidFields.push("Gender");
-        if(focusField === null){
-            focusField = form[2];
-        }
-    }
-    if( !validateMaritalStatus(form[4], form[5]) ) {
-        invalidFields.push("Marital Status");
-        if(focusField === null){
-            focusField = form[4];
-        }
-    }
-    else if( form[4].checked && !validateNameOfSpouse(form[6]) ) {
-        invalidFields.push("Name of Spouse");
-        if(focusField === null){
-            focusField = form[6];
-        }
-    }
-    if( !validateOtherDetails(form[7]) ) {
-        invalidFields.push("Other Details");
-        if(focusField === null){
-            focusField = form[7];
-        }
-    }
-    if( !validateTnc(form[8]) ) {
-        isTncAccepted = false;
-        if(focusField === null){
-            focusField = form[0];
-        }
-    }
-
-    let message = "";
-    if(invalidFields.length === 1){
-        message += "Please enter a " + invalidFields[0] + "\n";
-    }
-    else if(invalidFields.length > 1){
-        message += "Please enter ";
-        for (field in invalidFields) {
-            if(field == 0){
-                message += invalidFields[field];
-            }else if(field == invalidFields.length -1){
-                message += " and " + invalidFields[field];
-            }else{
-                message += ", " + invalidFields[field];
-            }
-        }
-        message += "\n";
-    }
-
-    if(!isTncAccepted){
-        message += "Please accept Terms and Conditions";
-    }
-    if(message.length !== 0){
+    if( (message = validateTextInput(form[0].value, "First Name")).length !== 0 ) {
+        form[0].focus();
         alert(message);
-        focusField.focus();
-    }
-    else{
+    }else if( (message = validateTextInput(form[1].value, "Last Name")).length !== 0 ) {
+        form[1].focus();
+        alert(message);
+    }else if( (message = validateOption(form[2], "Gender")).length !== 0 ) {
+        form[2].focus();
+        alert(message);
+    }else if( (message = validateOption(form[3], "Marital Status")).length !== 0 ) {
+        form[3].focus();
+        alert(message);
+    }else if(form[3][1].selected && (message = validateTextInput(form[4].value, "Name of Spouse")).length !== 0 ){
+        form[4].focus();
+        alert(message);
+    }else if( (message = validateTextArea(form[5].value, "Other Details")).length !== 0 ){
+        form[5].focus();
+        alert(message);
+    }else if( (message = validateTnc(form[6])).length !== 0){
+        form[6].focus();
+        alert(message);
+    }else{
         document.getElementById('modalFirstName').innerHTML = form[0].value;
         document.getElementById('modalLastName').innerHTML = form[1].value;
-        document.getElementById('modalGender').innerHTML = (form[2].checked) ? "Male" : "Female" ;
-        document.getElementById('modalMaritalStatus').innerHTML = (form[4].checked) ? "Married" : "Unmarried" ;
-        if(form[4].checked){
+        document.getElementById('modalGender').innerHTML = form[2].value;
+        document.getElementById('modalMaritalStatus').innerHTML = form[3].value;
+        if(form[3][1].selected){
             document.getElementById('modalNameOfSpouseEntry').style.display = "block";
-            document.getElementById('modalNameOfSpouse').innerHTML = form[6].value;
+            document.getElementById('modalNameOfSpouse').innerHTML = form[4].value;
         }else{
             document.getElementById('modalNameOfSpouseEntry').style.display = "none";
         }
-        document.getElementById('modalOtherDetails').innerHTML = form[7].value;
+        document.getElementById('modalOtherDetails').innerHTML = form[5].value;
         document.getElementById('modal').style.display = "block";
     }
 }
 
-function validateFirstName(inputText) {
-    const value = inputText.value;
-    if(value === ""){
-        return false;
+function isEmpty(str) { return (str.length === 0); }
+function containSpaces(str) { return (/\s/.test(str)) }
+
+function validateTextInput(inputText, fieldName) {
+    if(isEmpty(inputText)){
+        return "Please Enter " + fieldName;
     }
-    else if(/\s/.test(value)){
-        alert('First Name should not contain spaces');
-        inputText.focus();
-        return false;
+    else if(containSpaces(inputText)){
+        return fieldName + " should not contain spaces";
     }
-    return true;
+    return "";
 }
 
-function validateLastName(inputText) {
-    const value = inputText.value;
-    if(value === ""){
-        return false;
+function validateOption(input, fieldName) {
+    if( !(input[1].selected || input[2].selected) ){
+        return "Please select " + fieldName;
     }
-    else if(/\s/.test(value)){
-        alert('Last Name should not contain spaces');
-        inputText.focus();
-        return false;
-    }
-    return true;
+    return "";
 }
 
-function validateGender(input1, input2) {
-    if( !(input1.checked || input2.checked) ){
-        return false;
+function validateTextArea(inputText, fieldName) {
+    if(isEmpty(inputText)){
+        return "Please Enter " + fieldName;
     }
-    return true;
-}
-
-function validateMaritalStatus(input1, input2) {
-    if( !(input1.checked || input2.checked) ){
-        return false;
-    }
-    return true;
-}
-
-function validateNameOfSpouse(inputText) {
-    const value = inputText.value;
-    if(value === ""){
-        return false;
-    }
-    else if(/\s/.test(value)){
-        alert('Name of Spouse should not contain spaces');
-        inputText.focus();
-        return false;
-    }
-    return true;
-}
-
-function validateOtherDetails(inputText) {
-    const value = inputText.value;
-    if(value === ""){
-        return false;
-    }
-    return true;
+    return "";
 }
 
 function validateTnc(input) {
-    if( !(input.checked) ){
-        return false;
+    if(!input.checked) {
+        return "Please accept Terms and Conditions";
     }
-    return true;
+    return "";
 }
 
 function onMaritalStatusChange(event) {
-    const maritalStatus = document.getElementsByName('maritalStatus');
+    const maritalStatus = document.getElementById('marital-status-list')
     const nameOfSpouseText = document.getElementById('nameOfSpouse');
-    if(maritalStatus[0].checked){
+    if(maritalStatus[1].selected){
         nameOfSpouseText.disabled = false;
     }
     else{
